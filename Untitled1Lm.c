ab24a50f8144a4fd54360107fd02f47fcad6202e
LuisM12leon <leml090512hmcnrsa1@lake.edu.mx>
2025-10-21 13:09:07 -0600

    1-suma de tres numeros

--- Untitled1Lm.c
+++ Untitled1Lm.c
@@ -1,13 +1,21 @@
 #include<stdio.h>
 main(){
-float altura;
-float base;
-float area;
+float num1;
+float num2;
+float num3;
+float suma;
 
-printf("Ingresa la altura");
-scanf(",&altura");
-primtf("ingresa la base");
-scanf("%f",altura);
+printf("Ingresa el numero:\n");
+scanf("%f",&num1);
 
-area = (base*) /2;
-printf("El area del triangulo es: %.2f \n",area);
+printf("Ingresa el numero dos:\n");
+scanf("%f",&num2);
+
+
+printf("Ingresa el numero tres:\n");
+scanf("%f",&num3);
+suma = num1 + num2 + num3;
+
+printf("La suma de los 3 numero es: %.2f",suma);
+}
+
