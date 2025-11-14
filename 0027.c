d7971918833e79b36efc9a83644412d23acc02b0
LuisM12leon <leml090512hmcnrsa1@lake.edu.mx>
2025-11-14 09:53:10 -0600

    Update 0027.c

--- 0027.c
+++ 0027.c
@@ -1,14 +1,11 @@
 #include<stdio.h>
-main(){
-float altura;
-float base;
-float area;
 
-printf("Ingresa la altura\n");
-scanf("%f",&altura);
-printf("ingresa la base\n");
-scanf("%f",&base);
+main() {
+    float alturaTriangulo;
+    float baseTriangulo;
+    float areaTriangulo;
 
-area = (base*altura) / 2;
-printf("El area del triangulo es: %.2f \n", area);
-}
+    printf("Ingresa la altura\n");
+    scanf("%f", &alturaTriangulo);
+
+
